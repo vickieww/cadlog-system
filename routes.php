6d62300d747d25bad097f87d413d3bc9a0efe5f9
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-11 09:49:03 -0300

    Inclui arquivos de controlador para lidar com diferentes ações como listar usuários cadastrados, cadastrar os usuários, etc.

---
+++ routes.php
@@ -0,0 +1,6 @@
+<?php
+//Inclui arquivos de controlador para lidar com diferentes ações como, listar usuário cadastrados, cadastrar os usuários, etc.
+require 'controllers/AuthController.php'; //Inclui o controlador de autenticação
+require 'controllers/UserController.php'; //Inclui o controlador de usuário
+require 'controller/DashboardController.php'; //Inclui o controlador de dashboard
+?>
