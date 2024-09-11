7d791fc62711ac6f785b6124340013a727eedec6
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-11 10:17:22 -0300

    Direciona os dados coletados pelo login e outras páginas do site para o arquivo de autenticação AuthController.php

--- routes.php
+++ routes.php
@@ -3,4 +3,16 @@
 require 'controllers/AuthController.php'; //Inclui o controlador de autenticação
 require 'controllers/UserController.php'; //Inclui o controlador de usuário
 require 'controller/DashboardController.php'; //Inclui o controlador de dashboard
+
+//Cria instâncias dos controladores para utilizar os métodos
+$authController = new AuthController(); //Instancia o controlador de autenticação
+
+//Coleta a ação da url, se não houver ação definida, usa login por padrão
+$action = $_GET['action'] ?? 'login'; //Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+
+//Verifica a ação solicitada e chama o método apropriado do controlador
+switch($action){
+    case 'login':
+        $authController->login(); //Chama o método de login do controlador de autenticação
+}
 ?>
