6dc431fd446d6a9cd1e97f4cda4709cd1c55976e
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-17 12:10:51 -0300

    Altera nome da pasta para tentar solver erro de ligação dos arquivos.

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 //Inclui arquivos de controlador para lidar com diferentes ações como, listar usuário cadastrados, cadastrar os usuários, etc.
 require 'controllers/AuthController.php'; //Inclui o controlador de autenticação
 require 'controllers/UserController.php'; //Inclui o controlador de usuário
-require 'controller/DashboardController.php'; //Inclui o controlador de dashboard
+require 'controllers/DashboardController.php'; //Inclui o controlador de dashboard
 
 //Cria instâncias dos controladores para utilizar os métodos
 $authController = new AuthController(); //Instancia o controlador de autenticação
