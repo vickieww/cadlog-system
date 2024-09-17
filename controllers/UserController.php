23cdbe6e6cb44622eb26a74a6b42c3a77964043a
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-17 12:30:25 -0300

    Cria a função responsável por cadastrar o usuário.

---
+++ controllers/UserController.php
@@ -0,0 +1,9 @@
+<?php
+//Definição da classe UserController, responsável por gerenciar as ações relacionadas aos usuários
+class UserController{
+    //Cria a função responsável por cadastrar o usuário
+    public function register(){
+
+    }
+}
+?>
