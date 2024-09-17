8bd2a9afb02f25a9176167145c4b6d7ff5031666
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-17 12:49:06 -0300

    Atualiza arquivo utilizando padrão singleton

---
+++ models/database.php
@@ -0,0 +1,5 @@
+<?php
+class Database{
+    //Utiliza padrão singleton cujo objetivo é garantir que apenas uma única instância de classe seja criada duante a execução do programa, e que essa instância seja utilizada sempre que necessário
+}
+?>
