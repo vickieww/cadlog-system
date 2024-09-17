b4a8e3d815045d9e9865defcfc2283ca9ebc5332
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-17 12:49:43 -0300

    Cria função responsável por cadastrar o usuário.

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,7 +3,19 @@
 class UserController{
     //Cria a função responsável por cadastrar o usuário
     public function register(){
-
+    if($_SEVER['REQUEST_METHOD'] == 'POST'){
+       $data = [
+        'nome'=> $_POST['nome'],
+        'email'=> $_POST['email'],
+        //O método password_hash encripta a senha para uma linguagem de criptografia padrão(PASSWORD_DEFAULT) 
+        'senha'=>password_hash($_POST['senha'], PASSWORD_DEFAULT),
+        'perfil'=>$_POST['perfil']
+           ];
+           User::create($data);
+    }else{
+        //Se a requisição não for POST, por exemplo "GET", carrega a página de registro
+        include 'views/register.php';
+    }
     }
 }
 ?>
