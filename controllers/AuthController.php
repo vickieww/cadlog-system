097d42a35b1e2ec59be53907440f94446d762ae3
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-17 12:10:22 -0300

    Altera o nome dos arquivos para melhor ligação entre os arquivos.

---
+++ controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+//Requere arquivo 'user.php' que contém o model user com as funções para manipulação de dados de usuários
+require_once 'models/user.php';
+
+class AuthController{
+    //Cria função responsável pelo processo de login
+    public function login ()
+    {
+        
+    }
+}
+?>
