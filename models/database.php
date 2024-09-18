47d8cdecd3d0504f162943692b220b71ac5cb750
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-09-18 10:06:41 -0300

    Cria código de ligação e tratamento de erros com o Banco de Dados, configurando dados com o driver MySQL e as informações de host.

--- models/database.php
+++ models/database.php
@@ -1,5 +1,21 @@
 <?php
 class Database{
     //Utiliza padrão singleton cujo objetivo é garantir que apenas uma única instância de classe seja criada duante a execução do programa, e que essa instância seja utilizada sempre que necessário
-}
+    private static $instance =  null;
+    //Método público que retorna a conexão com o BD
+    public static function getConnection(){
+        //Verifica se a instância de conexão ainda não foi criada
+        if(!self::$instance){
+            $host ='localhost';
+            $db = 'sistema_usuarios';
+            $user = 'root';
+            $password = '';
+//A conexão utiliza o driver MySQL (mysql:) e as informações de host e DB
+            self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+//Define modo de erro para exceções, facilitando a depuração e tratamento do erro
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
+                return self::$instance;
+    }
+    }
 ?>
