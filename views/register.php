acbc2181c53bf37ca86137aca913bb183589c415
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-10-08 12:05:28 -0300

    Faz ligação do arquivo routes com register para realizar o registro do usuário com os dados informados.

--- views/register.php
+++ views/register.php
@@ -65,7 +65,7 @@ a {
 <body>
     <div>
         <h2>Cadastro de usuário</h2>
-        <form action="" method="post">
+        <form action="index.php?action=register" method="post">
             <label for = "nome">Nome</label>
             <input type = "text" name="nome" id="nome" required>
 
@@ -83,7 +83,7 @@ a {
         </select>
         <button type="submit">Cadastrar</button>
         </form>
-        <a href="">Voltar ao Login</a>
+        <a href="index.php?action=login">Voltar ao Login</a>
     </div>
 </body>
 </html>
