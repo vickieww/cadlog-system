fb175d1fe8489f08de16e6b56e37bf73ae648008
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-10-22 11:25:35 -0300

    Inicia a sessão para verificar se o usuário está autenticado e verifica se a variável de sessão "usuario_id" está definida, caso não estiver significa que o usuário não está autenticado

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -1,6 +1,17 @@
 <?php
 
 class DashboardController{
-    
+    public function index(){
+        //inicia a sessão para verificar se o usuário está autenticado
+        session_start();
+
+        //Verifica se a variável de sessão "usuario_id" está definida caso não estiver significa que o usuário não está autenticado
+        if (!isset($_SESSION['usuario_id'])){
+            header('Location: index.php');
+            exit; //garante que o script seja interrompido após o redirecionamento
+        }
+        include 'views/dashboard.php';
+    }
+
 }
 ?>
